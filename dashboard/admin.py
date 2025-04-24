c36f9afb5d340a294ab7dcf680adfd4a449cfb90
Ivan Nazaruk <nazaruk7649@ukr.net>
2025-04-24 20:55:42 +0100

    clean tracked files now ignored in .gitignore

--- dashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "category", "due_date", "is_completed")
-    list_filter = ("category", "is_completed", "due_date")
-    search_fields = ("title", "description", "user__username")
